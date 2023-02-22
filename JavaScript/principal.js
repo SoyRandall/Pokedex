1fa506910bcd77f6b29e3d05497099ab4f0eb9ae
Randall Aguilar <raam19911@gmail.com>
2023-02-22 08:23:00 -0600

    Correción barra estadísticas

--- JavaScript/principal.js
+++ JavaScript/principal.js
@@ -117,37 +117,37 @@ function removeChildNodes(parent) {
 function progressBars(stats) {
     const statsContainer = document.createElement("div");
     statsContainer.classList.add("stats-container");
-
-    for (let i = 0; i < 5; i++) {
-        const stat = stats[i];
-
-        const statPercent = stat.base_state / 2 + "%";
-        const statContainer = document.createElement("stat-container");
-        statContainer.classList.add("stat-container");
-
-        const statName = document.createElement("p");
-        statName.textContent = stat.stat.name;
-
-        const progress = document.createElement("div");
-        progress.classList.add("progress");
-
-        const progressBar = document.createElement("div");
-        progressBar.classList.add("progress-bar");
-        progressBar.setAttribute("aria-valuenow", stat.base_stat);
-        progressBar.setAttribute("aria-valuemin", 0);
-        progressBar.setAttribute("aria-valuemax", 200);
-        progressBar.style.width = statPercent;
-
-        progress.textContent = stat.base_stat;
-
-        progress.appendChild(progressBar);
-        statContainer.appendChild(statName);
-        statContainer.appendChild(progress);
-        
-        statsContainer.appendChild(statContainer);
+  
+    for (let i = 0; i < 3; i++) {
+      const stat = stats[i];
+  
+      const statPercent = stat.base_stat / 2 + "%";
+      const statContainer = document.createElement("stat-container");
+      statContainer.classList.add("stat-container");
+  
+      const statName = document.createElement("p");
+      statName.textContent = stat.stat.name;
+  
+      const progress = document.createElement("div");
+      progress.classList.add("progress");
+  
+      const progressBar = document.createElement("div");
+      progressBar.classList.add("progress-bar");
+      progressBar.setAttribute("aria-valuenow", stat.base_stat);
+      progressBar.setAttribute("aria-valuemin", 0);
+      progressBar.setAttribute("aria-valuemax", 200);
+      progressBar.style.width = statPercent;
+  
+      progressBar.textContent = stat.base_stat;
+  
+      progress.appendChild(progressBar);
+      statContainer.appendChild(statName);
+      statContainer.appendChild(progress);
+  
+      statsContainer.appendChild(statContainer);
     }
-
+  
     return statsContainer;
-}
+  }
 
 buscarPokemon(id);
